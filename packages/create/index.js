91596870fbc13de99428b16f7a9ef07d6f888617
mattcroat <mattcroat.dev@gmail.com>
2023-07-21 17:49:54 +0200

    chore: CLI

--- packages/create/index.js
+++ packages/create/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('index')
+import { text } from '@clack/prompts'
+
+const meaning = await text({
+	message: 'What is the meaning of life?',
+	placeholder: 'Not sure',
+	initialValue: '42',
+	validate(value) {
+		if (value.length === 0) return `Value is required!`
+	},
+})
